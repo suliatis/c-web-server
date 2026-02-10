7e535a4d74e025c2433e10ce93f2a7580b1b9c04
suliatis <suli.zakar.attila@gmail.com>
2026-02-10 16:20:38 +0100

    use str to parse request line

--- main.c
+++ main.c
@@ -7,12 +7,11 @@
 #include <sys/uio.h>
 #include <unistd.h>
 
-#include "src/str.c"
 #include "src/log.c"
+#include "src/str.c"
 
-// TODO: use str struct instead of char
-// TODO: create an str constructor for literals
-static const char HTTP_HEADERS_END[] = "\r\n\r\n";
+static const struct Str HTTP_HEADERS_END = STR("\r\n\r\n");
+static const struct Str HTTP_REQUEST_METHOD_GET = STR("GET");
 
 int main(void) {
   int sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -42,48 +41,61 @@ int main(void) {
   // FIXME: handle str_create and str_recv(0 -> connection closed, -1 -> error)
   // errors
   // FIXME: free request_header
-  struct Str request_header = {0};
-  str_create(&request_header, 256);
+  struct Str http_request_header = {0};
+  str_create(&http_request_header, 256);
   // TODO: while bytes recvd or limit (8192) reached
-  while (!str_ends_with(&request_header, HTTP_HEADERS_END,
-                        strlen(HTTP_HEADERS_END))) {
-    ssize_t bytes_recvd = str_recv(&request_header, accepted, 256);
+  while (!str_ends_with(&http_request_header, &HTTP_HEADERS_END)) {
+    ssize_t bytes_recvd = str_recv(&http_request_header, accepted, 256);
     log_info("Reading request header from %d, bytes received: %zd", accepted,
              bytes_recvd);
   }
 
-  char method[16];
-  char path[256];
-  char protocol[16];
-  int words =
-      sscanf(request_header.data, "%15s /%255s %15s", method, path, protocol);
-  if (words < 3) {
-    log_error("Malformed request: %s", request_header.data);
+  struct StrCursor http_request_header_cursor = {0};
+  str_cursor_init(&http_request_header_cursor, &http_request_header);
+  struct Str request_line = str_cursor_next(&http_request_header_cursor, &STR_NL);
 
-    char *statusline = "HTTP/1.1 400 Bad Request\r\n";
-    char *contenttype = "Content-Type: text/html\r\n";
-    char *connection = "Connection: close\r\n";
-    char *empty = "\r\n";
-    char body[256];
-    sprintf(body, "Malformed request: %s", request_header.data);
+  struct StrCursor http_request_line_cursor = {0};
+  str_cursor_init(&http_request_line_cursor, &request_line);
 
-    write(accepted, statusline, strlen(statusline));
-    write(accepted, contenttype, strlen(contenttype));
-    write(accepted, connection, strlen(connection));
-    write(accepted, empty, strlen(empty));
-    write(accepted, body, strlen(body));
+  struct Str http_request_method =
+      str_cursor_next(&http_request_line_cursor, &STR_SPC);
+  struct Str http_request_path =
+      str_cursor_next(&http_request_line_cursor, &STR_SPC);
+  struct Str http_request_version =
+      str_cursor_next(&http_request_line_cursor, &STR_SPC);
 
-    goto close_all;
-  }
+  if (str_is_empty(&http_request_method) || str_is_empty(&http_request_path) ||
+      str_is_empty(&http_request_version)) {
+      log_error("Malformed request: %s", http_request_header.data);
 
-  if (strcmp(method, "GET") == 0) {
-    char static_path[256];
-    sprintf(static_path, "static/%s", path);
-    int open_file = open(static_path, O_RDONLY);
+      char *statusline = "HTTP/1.1 400 Bad Request\r\n";
+      char *contenttype = "Content-Type: text/html\r\n";
+      char *connection = "Connection: close\r\n";
+      char *empty = "\r\n";
+      char body[256];
+      sprintf(body, "Malformed request: %s", http_request_header.data);
+
+      write(accepted, statusline, strlen(statusline));
+      write(accepted, contenttype, strlen(contenttype));
+      write(accepted, connection, strlen(connection));
+      write(accepted, empty, strlen(empty));
+      write(accepted, body, strlen(body));
+
+      goto close_all;
+    }
+
+  if (str_equals(&http_request_method, &HTTP_REQUEST_METHOD_GET)) {
+    struct Str static_path = {0};
+    struct Str static_path_prefix = STR("static/");
+    str_copy(&static_path, &http_request_path);
+    str_prepend(&static_path, &static_path_prefix);
+
+
+    int open_file = open(static_path.data, O_RDONLY);
     if (open_file >= 0) {
-      log_info("Static resource opened: %d, %s", open_file, path);
+      log_info("Static resource opened: %d, %s", open_file, static_path.data);
     } else {
-      log_error("Opening static resource failed: %s with %s", path,
+      log_error("Opening static resource failed: %s with %s", static_path.data,
                 strerror(errno));
 
       char *statusline = "HTTP/1.1 404 Not Found\r\n";
@@ -91,7 +103,7 @@ int main(void) {
       char *connection = "Connection: close\r\n";
       char *empty = "\r\n";
       char body[256];
-      sprintf(body, "File not found: %s", path);
+      sprintf(body, "File not found: %s", static_path.data);
 
       write(accepted, statusline, strlen(statusline));
       write(accepted, contenttype, strlen(contenttype));
@@ -112,13 +124,14 @@ int main(void) {
     write(accepted, connection, strlen(connection));
     write(accepted, empty, strlen(empty));
 
-    char read_buffer[256] = {0};
-    ssize_t bytes_read = 1;
+    ssize_t bytes_read_size = 1;
     ssize_t total_bytes = 0;
-    while (bytes_read > 0) {
-      bytes_read = read(open_file, read_buffer, 256);
-      // FIXME: check if bytes read is greater or equal to 0 then cast it to unsigned
-      ssize_t bytes_wrote = write(accepted, read_buffer, bytes_read);
+    size_t chunk_size = 256;
+    while (bytes_read_size > 0) {
+      struct Str buffer = {0};
+      str_create(&buffer, chunk_size);
+      bytes_read_size = str_read(&buffer, open_file, chunk_size);
+      ssize_t bytes_wrote = str_write(&buffer, open_file);
       total_bytes = total_bytes + bytes_wrote;
     }
     log_info("Total bytes wrote to the client: %d %zd bytes", accepted,
@@ -126,7 +139,7 @@ int main(void) {
 
     close(open_file);
   } else {
-    log_error("Method not allowed: %s", method);
+    log_error("Method not allowed: %s", http_request_method.data);
 
     char *statusline = "HTTP/1.1 405 Method Not Allowed\r\n";
     char *contenttype = "Content-Type: text/html\r\n";
@@ -134,7 +147,9 @@ int main(void) {
     char *allow = "Allow: GET\r\n";
     char *empty = "\r\n";
     char body[256];
-    sprintf(body, "Method not allowed: %s", method);
+    // FIXME: because http_request_method is a view getting the raw data
+    // will return the whole string no matter what.
+    sprintf(body, "Method not allowed: %s", http_request_method.data);
 
     write(accepted, statusline, strlen(statusline));
     write(accepted, contenttype, strlen(contenttype));
--- src/str.c
+++ src/str.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/_types/_ssize_t.h>
+#include <unistd.h>
 #include <sys/socket.h>
 
 struct Str {
@@ -24,15 +25,70 @@ int str_create(struct Str *str, size_t cap) {
   return 0;
 }
 
+#define STR(s) (struct Str){.data = (char *)s, .len = sizeof(s) - 1, .cap = 0}
+
+const struct Str STR_NL = STR("\r\n");
+const struct Str STR_SPC = STR(" ");
+
+// TODO: add consts for empty and/or null strs
+bool str_is_readonly(const struct Str *str) { return str->cap == 0; }
+
+bool str_is_empty(const struct Str *str) { return str->len == 0; }
+
+bool str_equals(const struct Str *left, const struct Str *right) {
+  return memcmp(left->data, right->data, left->len) == 0;
+}
+
+bool str_starts_with(const struct Str *str, const struct Str *prefix) {
+  if (str->len < prefix->len) {
+    return false;
+  }
+
+  return memcmp(str->data, prefix->data, prefix->len) == 0;
+}
+
+bool str_ends_with(const struct Str *str, const struct Str *suffix) {
+  if (str->len < suffix->len) {
+    return false;
+  }
+
+  char *data = &str->data[str->len - suffix->len];
+  return memcmp(data, suffix->data, suffix->len) == 0;
+}
+
+size_t str_get_bytes(const struct Str *str, char *out, size_t len) {
+  size_t min_len = len;
+  if (str->len < len) {
+    min_len = str->len;
+  }
+  memcpy(out, str->data, min_len);
+  return min_len;
+}
+
 void str_free(struct Str *str) {
+  if (str_is_readonly(str)) {
+    return;
+  }
+
   free(str->data);
   str->data = NULL;
   str->len = 0;
   str->cap = 0;
 }
 
+ssize_t str_copy(struct Str *dst, const struct Str *src) {
+  int created = str_create(dst, src->cap);
+  if (created < 0) {
+    return created;
+  }
+
+  memcpy(dst->data, src->data, src->len);
+  dst->len = src->len;
+  return (ssize_t)dst->len;
+}
+
 // TODO: keep str null terminated by reserving an extra byte if necessary
-ssize_t str_append(struct Str *str, const char *bytes, size_t len) {
+ssize_t str_append_char(struct Str *str, const char *bytes, size_t len) {
   size_t nxt_len = str->len + len;
   size_t nxt_cap = str->cap;
   char *nxt_data = str->data;
@@ -58,6 +114,57 @@ ssize_t str_append(struct Str *str, const char *bytes, size_t len) {
   return (ssize_t)len;
 }
 
+ssize_t str_append(struct Str *str, const struct Str *postfix) {
+  char *postfix_bytes = malloc(postfix->len);
+  if (postfix_bytes == NULL) {
+    return -1;
+  }
+
+  size_t postfix_len = str_get_bytes(postfix, postfix_bytes, postfix->len);
+  ssize_t res = str_append_char(str, postfix_bytes, postfix_len);
+  free(postfix_bytes);
+  return res;
+}
+
+ssize_t str_prepend_char(struct Str *str, const char *bytes, size_t len) {
+  size_t nxt_len = str->len + len;
+  size_t nxt_cap = str->cap;
+  char *nxt_data = str->data;
+
+  if (nxt_len > str->cap) {
+    nxt_cap = str->cap * 2;
+    if (nxt_len > nxt_cap) {
+      nxt_cap = nxt_len * 2;
+    }
+
+    nxt_data = realloc(str->data, nxt_cap);
+    if (nxt_data == NULL) {
+      return -1;
+    }
+  }
+
+  memmove(&nxt_data[len], nxt_data, str->len);
+  memcpy(nxt_data, bytes, len);
+
+  str->len = nxt_len;
+  str->cap = nxt_cap;
+  str->data = nxt_data;
+
+  return (ssize_t)len;
+}
+
+ssize_t str_prepend(struct Str *str, const struct Str *prefix) {
+  char *prefix_bytes = malloc(prefix->len);
+  if (prefix_bytes == NULL) {
+    return -1;
+  }
+
+  size_t prefix_len = str_get_bytes(prefix, prefix_bytes, prefix->len);
+  ssize_t res = str_prepend_char(str, prefix_bytes, prefix_len);
+  free(prefix_bytes);
+  return res;
+}
+
 ssize_t str_recv(struct Str *str, int fd, size_t len) {
   char *buffer = malloc(len);
   if (buffer == NULL) {
@@ -70,7 +177,7 @@ ssize_t str_recv(struct Str *str, int fd, size_t len) {
     return -1;
   }
 
-  ssize_t bytes_appended = str_append(str, buffer, (size_t)bytes_recvd);
+  ssize_t bytes_appended = str_append_char(str, buffer, (size_t)bytes_recvd);
   if (bytes_appended < 0) {
     free(buffer);
     return -1;
@@ -80,11 +187,99 @@ ssize_t str_recv(struct Str *str, int fd, size_t len) {
   return bytes_appended;
 }
 
-bool str_ends_with(const struct Str *str, const char *needle, size_t len) {
-  if (str->len < len) {
-    return false;
+
+ssize_t str_read(struct Str *out, int fd, size_t len) {
+  char *buffer = malloc(len);
+  if (buffer == NULL) {
+    return -1;
   }
 
-  char *data = &str->data[str->len - len];
-  return memcmp(data, needle, len) == 0;
+  ssize_t bytes_read = read(fd, buffer, len - 1);
+  if (bytes_read < 0) {
+    free(buffer);
+    return -1;
+  }
+
+  ssize_t bytes_appended = str_append_char(out, buffer, (size_t)bytes_read);
+  if (bytes_appended < 0) {
+    free(buffer);
+    return -1;
+  }
+
+  free(buffer);
+  return bytes_appended;
+}
+
+ssize_t str_write(const struct Str *str, int fd) {
+  char *buffer = malloc(str->len);
+  if (buffer == NULL) {
+    return -1;
+  }
+
+  size_t buffer_len = str_get_bytes(str, buffer, str->len);
+  ssize_t bytes_wrote = write(fd, buffer, buffer_len);
+  free(buffer);
+  return bytes_wrote;
+}
+
+struct StrCursor {
+  const char *data;
+  size_t len;
+};
+
+void str_cursor_init(struct StrCursor *cursor, const struct Str *str) {
+  cursor->data = str->data;
+  cursor->len = str->len;
+}
+
+struct Str str_cursor_view(const struct StrCursor *cursor) {
+  struct Str view = {
+      .data = (char *)cursor->data,
+      .len = cursor->len,
+      .cap = 0,
+  };
+
+  return view;
+}
+
+size_t str_cursor_inc(struct StrCursor *out) {
+  if (out->len == 0) {
+    return 0;
+  }
+
+  out->data = &out->data[1];
+  out->len = out->len - 1;
+  return out->len;
+}
+
+size_t str_cursor_inc_by(struct StrCursor *out, size_t by) {
+  while (by > 0) {
+    by = by - 1;
+    str_cursor_inc(out);
+  }
+
+  return out->len;
+}
+
+struct Str str_cursor_next(struct StrCursor *cursor, const struct Str *delim) {
+  size_t steps = 0;
+  struct Str out = {
+      .data = (char *)cursor->data,
+      .len = cursor->len,
+      .cap = 0,
+  };
+
+  while (cursor->len > 0) {
+    struct Str view = str_cursor_view(cursor);
+    if (str_starts_with(&view, delim)) {
+      str_cursor_inc_by(cursor, delim->len);
+      break;
+    }
+
+    str_cursor_inc(cursor);
+    steps = steps + 1;
+  }
+
+  out.len = steps;
+  return out;
 }
